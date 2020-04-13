53b546caee70d646cab4b5c28544c4393fb29195
Alex <cwensley@gmail.com>
2020-04-13 17:58:48 -0600

    added more iconography and updated resume links

--- src/organisms/ContactInfo.jsx
+++ src/organisms/ContactInfo.jsx
@@ -27,11 +27,18 @@ const ContactInfo = () => (
                         &nbsp;Email:&nbsp;alex@wensley.dev
                     </li><br/>
                 </ul>
-                <a  href="https://drive.google.com/file/d/1WOuEbYEf_MwZpesFB6iO-edCiSdyB8dD/edit"
+                <a  href="https://drive.google.com/uc?export=download&id=1lglRstTxyR3cgPvq90RcZuVOHOiial1D"
+                    className="btn btn-secondary p-1 w-75">
+                    <i className="fas mr-2 fa-file-pdf"></i>
+                    Download Resume
+                </a>
+                <br/>
+                <a  href="https://docs.google.com/document/d/1jRLh4DvLypQah-dIpQKaFFLboLc_7N6IPy5vSf0Akxs/edit?usp=sharing"
+                    className="o-ContactInfo-smallFontsize"
                     rel="noopener noreferrer"
-                    target="_blank"
-                    className="bg-secondary p-1">
-                    Download Resume from Google Drive
+                    target="_blank">
+                    Resume available in other formats via Google Drive
+                    <i className="fab ml-1 fa-google-drive"></i>
                 </a>
                 <ul className="social border border-secondary row px-0 py-2 mx-1 mt-4">
                     <li className="col-lg-6">
@@ -47,20 +54,35 @@ const ContactInfo = () => (
                         </a>
                     </li>
                 </ul>
-                <ul className="text-left pt-1">
-                    <li>
+                <ul className="social text-center pt-2 row">
+                    <li className="col-md-4">
                         <a href="https://cawensley.github.io/clean-energy/#/" rel="noopener noreferrer" target="_blank">
-                            Clean Energy Website
+                            <i className="fas fa-bolt mr-2 o-ContactInfo-iconwidth"></i>
+                            Clean Energy
+                            <br/>
+                            <img src={require('../images/CleanEnergyWebsite.png')}
+                                 alt=""
+                                 className="mt-2 mb-4 mb-md-0 border border-primary o-ContactInfo-PhotoWidth"/>
                         </a>
                     </li>
-                    <li>
+                    <li className="col-md-4">
                         <a href="https://cawensley.github.io/moviegallery/#/" rel="noopener noreferrer" target="_blank">
-                            Movie Gallery Website
+                            <i className="fas fa-film mr-2 o-ContactInfo-iconwidth"></i>
+                            Movie Gallery
+                            <br/>
+                            <img src={require('../images/MovieWebsite.png')}
+                                 alt=""
+                                 className="mt-2 mb-4 mb-md-0 border border-primary o-ContactInfo-PhotoWidth"/>
                         </a>
                     </li>
-                    <li>
+                    <li className="col-md-4">
                         <a href="https://www.warhammer40kbuilder.info/#/" rel="noopener noreferrer" target="_blank">
-                            Warhammer Website
+                            <i className="fas fa-dice-six mr-2 o-ContactInfo-iconwidth"></i>
+                            Warhammer 40k
+                            <br/>
+                            <img src={require('../images/WarhammerWebsite.png')}
+                                 alt=""
+                                 className="mt-2 mb-4 mb-md-0 border border-primary o-ContactInfo-PhotoWidth"/>
                         </a>
                     </li>
                 </ul>
