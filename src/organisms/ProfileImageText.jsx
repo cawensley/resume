3309c4f87c89ca199a24157e4b18aedcbb7680f4
Alex <cwensley@gmail.com>
2020-07-17 01:09:56 -0600

    updated Angular experience

--- src/organisms/ProfileImageText.jsx
+++ src/organisms/ProfileImageText.jsx
@@ -4,7 +4,7 @@ const ProfileImageText = () => (
     <div className="text-white">
         <h4 className="pt-4">Alex Wensley</h4>
         <h5 className="text-secondary mt-2 mb-3">Front-end Web Developer</h5>
-        <p className="border-top pt-3">"I have experience with React, Redux, Node.js, Bootstrap, and Jest/Enzyme"</p>
+        <p className="border-top pt-3">"I have experience with Angular, React, Redux, Node.js, Bootstrap, and Jest/Enzyme"</p>
     </div>
 );
 
